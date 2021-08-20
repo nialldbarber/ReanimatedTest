3970659f53457d77109b4ef1137e23cea9ad9d16
Niall Barber <niall.barber@actual-experience.com>
2021-08-20 07:03:03 +0100

    Componentised code

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,8 +1,8 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {PINKISH} from '../screens/HomeScreen';
+import {PINKISH} from './GestureStuff';
 import {RootStackParamList} from '../App';
 
 type TestScreenProp = StackNavigationProp<RootStackParamList, 'Test'>;
